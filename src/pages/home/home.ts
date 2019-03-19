08dd08ce4214eb692799240924142e4452c7b6e9
Paul Avila <pavil13@safeway.com>
2019-03-18 17:19:37 -0700

    api update

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ApiProvider } from '../../providers/api/api';
+import { CodegenComponentFactoryResolver } from '@angular/core/src/linker/component_factory_resolver';
 
 @Component({
   selector: 'page-home',
@@ -13,34 +14,9 @@ export class HomePage {
 
   }
 
-APIurl:string = 'https://api.github.com/'
-search:any = 'softstackfactory'
-out:any
-client_Id = '77b4f0d9a42a38fe7949'
-client_Secret = 'bb041c31307f2b955ccc0a62a5022fea2b9185fa'
 
-  searchOrgs() {
-
-    fetch(this.APIurl + 'orgs/' + this.search + '/repos?sort=created')
-    .then(response => response.json())
-    .then(json => {
-        this.out = json
-        console.log(this.out)
-        })
+  ionViewDidEnter() {
+    this.apiProvider.accessToken()
   }
 
-  signIn() {
-    window.location.href='https://github.com/login/oauth/authorize' + '?client_id=' + this.client_Id + '&scope=repo';
-  }
-
-  // postIssue() {
-
-  //   fetch('https://api.github.com/repos/Avilpa1/gitIssue/issues?access_token=e7a9ba9905a268cfd0d45d74a429b694124bc802', {
-  //     method: 'post',
-  //     body: {
-  //       title: 'Title',
-  //       body: {body: "body", title: "title"}
-  //     }
-  //   })
-  // }
 }
--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -10,11 +10,16 @@ export class ApiProvider {
   }
 
   githubURL:string = 'https://api.github.com/'
+  githubTokenUrl:string = 'https://cors-anywhere.herokuapp.com/https://github.com/login/oauth/access_token?client_id='
   search:any = 'softstackfactory'
-  out:any
+  results:any = {
+    access_token: ''
+  }
   client_Id:any = '77b4f0d9a42a38fe7949'
   client_Secret:any = 'bb041c31307f2b955ccc0a62a5022fea2b9185fa'
-
+  code:any = {}
+  token:any 
+  
   user:string = 'Avilpa1';
   repo:string = 'gitIssueIonic';
 
@@ -32,38 +37,65 @@ export class ApiProvider {
 
   httpOptions = {
     headers: new HttpHeaders({
-      'Content-Type':  'application/json',
-      'Authorization': 'Basic ' + btoa('avilpa1:4130032Qa1!') 
+      'Accept':  'application/json',
+      'Origin': 'X-Requested-With'
     })
   };
 
   signInWithGithub() {
-    // window.location.href='https://github.com/login/oauth/authorize' + '?client_id=' + this.client_Id + '&scope=repo';
-    console.log('sign in')
-    return this.http.get( 'https://cors-anywhere.herokuapp.com/https://github.com/login/oauth/authorize' + '?client_id=' + this.client_Id + '&scope=repo')
+    window.location.href='https://github.com/login/oauth/authorize' + '?client_id=' + this.client_Id + '&scope=repo,user';
   }
 
-  getAccessTokenFromGithub() {
+  getCodeFromGithub() {
+    let url = window.location.href
 
+    let getParams = function (url) {
+      let params = {};
+      let parser = document.createElement('a');
+      parser.href = url;
+      let query = parser.search.substring(1);
+      let vars = query.split('&');
+      for (let i = 0; i < vars.length; i++) {
+        let pair = vars[i].split('=');
+        params[pair[0]] = decodeURIComponent(pair[1]);
+      }
+      return params;
+    };
+    
+    this.code = getParams(url)
+    console.log(this.code.code)
   }
-
-
-  postIssueURL:string = 'repos/organization/repo/issues'
-
-  postIssueToUsersRepo() { 
-    return this.http.post( this.githubURL + this.postIssueURL, this.body, this.httpOptions )
-  };
   
-  postIssue() {
-    console.log(this.body)
-      this.signInWithGithub()
+  getAccessTokenFromGithub() {
+    return this.http.post( this.githubTokenUrl + this.client_Id + '&client_secret=' + this.client_Secret + '&code=' + this.code.code, {}, this.httpOptions )
+  }
+
+  accessToken() {
+    this.getCodeFromGithub()
+      this.getAccessTokenFromGithub()
         .subscribe( (response) =>  {
-          let signUpResult = response
-          console.log(signUpResult)
-        
+          this.results = response
+          this.token = this.results.access_token
+          console.log(this.token)  
+          // this.postIssue()
     })
   }
+ 
+  postIssueURL:string = 'repos/' + this.user + '/' + this.repo + '/' + 'issues'
+  githubURL1:string = 'https://cors-anywhere.herokuapp.com/https://api.github.com/user'
 
+  postIssueToUsersRepo() { 
+    console.log(this.githubURL1 + '?access_token=' + this.token)
+    return this.http.post( this.githubURL1 + this.postIssueURL + '?access_token=' + this.token , this.body, this.httpOptions )
+  };
 
+  postIssue() {
+    this.postIssueToUsersRepo()
+      this.getAccessTokenFromGithub()
+        .subscribe( (response) =>  {
+          let out = response
+          console.log(out)   
+    })
+  }
 
 }
